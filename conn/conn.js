21e7d8ee6251f4a9d151129382f11a70c87a20db
BrunaCardoso7 <obitadrawing@gmail.com>
2023-11-02 15:09:22 -0300

    ainda deploy no render

--- conn/conn.js
+++ conn/conn.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose')
 require('dotenv').config
 
 const password = process.env.PASSWORD
+
 mongoose.set('strictQuery', true)
 async function main(){
     await mongoose.connect(`mongodb+srv://obitadrawing:${password}@cluster0.kilvn6u.mongodb.net/?retryWrites=true&w=majority`)
     console.log('conexão com banco de dados estabelecida')
     
 }
-main().catch(console.log('error na conexão'))
+main().catch(console.error('error na conexão'))
 
 module.exports = main
