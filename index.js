6ceb4712691925115c47daa8053c184a418fe899
BrunaCardoso7 <obitadrawing@gmail.com>
2023-11-02 22:45:46 -0300

    reconstrução de midleware

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const cookieParser = require('cookie-parser');
+// ...
 app.use(cors())
+app.use(cookieParser());
 
 require("dotenv").config()
 
