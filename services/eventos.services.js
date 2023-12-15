8082e0cb0046ef7110c297525c4fedce954d5454
BrunaCardoso7 <obitadrawing@gmail.com>
2023-12-14 23:43:20 -0300

    feat: adjusted service userId population

--- services/eventos.services.js
+++ services/eventos.services.js
@@ -2,20 +2,20 @@ import { raw } from "express";
 import evento from "../model/evento.js";
 
 export const createService = (body)=>  evento.create(body)
-export const findAllService = (offset, limit)=>  evento.find().sort({_id: -1}).skip(offset).limit(limit).populate("usuario_id")
+export const findAllService = (offset, limit)=>  evento.find().sort({_id: -1}).skip(offset).limit(limit).populate("userId")
 export const countEvents = ()=> evento.countDocuments()
-export const topEventsService = () => evento.findOne().sort({_id: -1}).populate("usuario_id")
-export const findByIdEvetnService = (id)=> evento.findById(id).populate("usuario_id")
+export const topEventsService = () => evento.findOne().sort({_id: -1}).populate("userId")
+export const findByIdEvetnService = (id)=> evento.findById(id).populate("userId")
 export const findByTitleService = (title) => {
     return evento.find({
         nome: { $regex: new RegExp(title, 'i') }
-    }).sort({ _id: -1 }).populate("usuario_id");
+    }).sort({ _id: -1 }).populate("userId");
 };
 export const updateService = (id, nome, descricao, data, categoria, localizacao, image)=> evento.findOneAndUpdate({_id: id}, {nome, descricao, data, categoria, localizacao, image}, {rawResult: true})
 export const deleteService = (id)=> evento.findByIdAndDelete({_id: id})
 export const likeEventService = (idPost, userId) => evento.findByIdAndUpdate({_id: idPost, "like.userId":{nin: [userId]}},{$push: {like: {userId, created: new Date()}}}) 
 export const deleteLikeEventService = (idPost, userId)=> evento.findByIdAndUpdate({_id: idPost}, {$push: {like: {userId}}}) 
-export const byUserService = (id)=> evento.find({usuario_id: id}).sort({_id: -1}).populate("usuario_id")
+export const byUserService = (id)=> evento.find({usuario_id: id}).sort({_id: -1}).populate("userId")
 export const addCommentService = (idPost, comment, userId)=> {
     const idComment = Math.floor(Date.now()*Math.random()).toString(36)
     return evento.findOneAndUpdate({_id: idPost}, {$push: {comentario: {idComment, userId, comment, createdAt: new Date()}}})
